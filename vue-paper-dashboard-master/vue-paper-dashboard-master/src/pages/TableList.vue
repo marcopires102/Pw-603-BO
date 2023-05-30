9499d6caf1f8763c25a82cdb569a3e1af7f788fe
marcopires102 <marcopires2002@gmail.com>
2023-05-31 00:14:38 +0100

    ola

--- vue-paper-dashboard-master/vue-paper-dashboard-master/src/pages/TableList.vue
+++ vue-paper-dashboard-master/vue-paper-dashboard-master/src/pages/TableList.vue
@@ -1,70 +1,38 @@
 <template>
-  <div class="row">
+  <div>
     <div class="col-12">
-      <card :title="table1.title" :subTitle="table1.subTitle">
-        <div slot="raw-content" class="table-responsive">
-          <paper-table :data="table1.data" :columns="table1.columns">
-          </paper-table>
-        </div>
-      </card>
-    </div>
-
-    <div class="col-12">
-      <card class="card-plain">
+      <div class="card-plain">
         <div class="table-full-width table-responsive">
-          <paper-table
-            type="hover"
-            :title="table2.title"
-            :sub-title="table2.subTitle"
-            :data="table2.data"
-            :columns="table2.columns"
-          >
-          </paper-table>
+          <table class="table">
+            <thead>
+              <tr>
+                <th v-for="column in table2.columns" :key="column">{{ column }}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(row, index) in table2.data" :key="index">
+                <td v-for="(value, key) in row" :key="key">{{ value }}</td>
+                <td>
+                  <button @click="deleteUtilizador(index)">Apagar Utilizador</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-      </card>
+      </div>
     </div>
   </div>
 </template>
+
+
+
+
+
 <script>
+
+
+
 import { PaperTable } from "@/components";
-const tableColumns = ["Id", "Name", "Salary", "Country", "City"];
-const tableData = [
-  {
-    id: 1,
-    name: "Dakota Rice",
-    salary: "$36.738",
-    country: "Niger",
-    city: "Oud-Turnhout",
-  },
-  {
-    id: 2,
-    name: "Minerva Hooper",
-    salary: "$23,789",
-    country: "Curaçao",
-    city: "Sinaai-Waas",
-  },
-  {
-    id: 3,
-    name: "Sage Rodriguez",
-    salary: "$56,142",
-    country: "Netherlands",
-    city: "Baileux",
-  },
-  {
-    id: 4,
-    name: "Philip Chaney",
-    salary: "$38,735",
-    country: "Korea, South",
-    city: "Overland Park",
-  },
-  {
-    id: 5,
-    name: "Doris Greene",
-    salary: "$63,542",
-    country: "Malawi",
-    city: "Feldkirchen in Kärnten",
-  },
-];
 
 export default {
   components: {
@@ -72,19 +40,20 @@ export default {
   },
   data() {
     return {
-      table1: {
-        title: "Stripped Table",
-        subTitle: "Here is a subtitle for this table",
-        columns: [...tableColumns],
-        data: [...tableData],
-      },
       table2: {
         title: "Table on Plain Background",
         subTitle: "Here is a subtitle for this table",
-        columns: [...tableColumns],
-        data: [...tableData],
+        columns: ["Username", "Nome", "Telefone", "Tour", "Numero Participantes","Data","Horas","Preço","Estado"],
+        data: [],
       },
     };
+  },
+  mounted() {
+    // Retrieve "reservas" data from local storage
+    const storedRegistros = localStorage.getItem("registros");
+    if (storedRegistros) {
+      this.table2.data = JSON.parse(storedRegistros);
+    }
   },
 };
 </script>

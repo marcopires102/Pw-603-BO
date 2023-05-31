dfda2ce5064163758581db7ffc64862e98d1e1dd
marcopires102 <marcopires2002@gmail.com>
2023-05-31 01:15:21 +0100

    LUME

--- vue-paper-dashboard-master/vue-paper-dashboard-master/src/pages/TableList.vue
+++ vue-paper-dashboard-master/vue-paper-dashboard-master/src/pages/TableList.vue
@@ -11,7 +11,19 @@
             </thead>
             <tbody>
               <tr v-for="(row, index) in table2.data" :key="index">
-                <td v-for="(value, key) in row" :key="key">{{ value }}</td>
+                <td v-for="(value, key) in row" :key="key">
+                  <!-- Renderizar dropdown somente para a coluna "Estado" -->
+                  <template v-if="key === 'Estado'">
+                    <select @change="updateEstado(index, $event.target.value)" v-model="row.Estado">
+                      <option value="Confirmado">Confirmado</option>
+                      <option value="Pendente">Pendente</option>
+                    </select>
+                  </template>
+                  <!-- Renderizar valor para as outras colunas -->
+                  <template v-else>
+                    {{ value }}
+                  </template>
+                </td>
               </tr>
             </tbody>
           </table>
@@ -21,37 +33,73 @@
   </div>
 </template>
 
-
-
-
-
 <script>
-
-
-
-import { PaperTable } from "@/components";
-
 export default {
-  components: {
-    PaperTable,
-  },
   data() {
     return {
       table2: {
         title: "Table on Plain Background",
         subTitle: "Here is a subtitle for this table",
-        columns: ["Username", "Nome", "Telefone", "Tour", "Numero Participantes","Data","Horas","Preço","Estado"],
-        data: [],
-      },
+        columns: [
+          "Username",
+          "Nome",
+          "Telefone",
+          "Tour",
+          "Numero Participantes",
+          "Data",
+          "Horas",
+          "Preço",
+          "Estado"
+        ],
+        data: []
+      }
     };
   },
   mounted() {
-    // Retrieve "reservas" data from local storage
+    // Retrieve "registros" data from local storage
     const storedRegistros = localStorage.getItem("registros");
     if (storedRegistros) {
       this.table2.data = JSON.parse(storedRegistros);
+    } else {
+      // Fallback data if no data in local storage
+      this.table2.data = [
+        {
+          Username: "user1",
+          Nome: "Nome 1",
+          Telefone: "123456789",
+          Tour: "Tour A",
+          "Numero Participantes": 5,
+          Data: "2023-06-01",
+          Horas: "10:00",
+          Preço: 50,
+          Estado: "Confirmado"
+        },
+        {
+          Username: "user2",
+          Nome: "Nome 2",
+          Telefone: "987654321",
+          Tour: "Tour B",
+          "Numero Participantes": 3,
+          Data: "2023-06-02",
+          Horas: "14:00",
+          Preço: 40,
+          Estado: "Pendente"
+        }
+        // Add more data objects as needed
+      ];
+
+      // Save data to local storage
+      localStorage.setItem("registros", JSON.stringify(this.table2.data));
     }
   },
+  methods: {
+    updateEstado(index, value) {
+      // Atualizar o valor "Estado" do item selecionado e salvar no Local Storage
+      this.table2.data[index].Estado = value;
+      localStorage.setItem("registros", JSON.stringify(this.table2.data));
+    }
+  }
 };
 </script>
+
 <style></style>

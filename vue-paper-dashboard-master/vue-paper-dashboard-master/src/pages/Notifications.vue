a6c0d36209d14c75474edc3abac13797abafd5b9
marcopires102 <marcopires2002@gmail.com>
2023-05-31 04:07:36 +0100

    impares sao merda

--- vue-paper-dashboard-master/vue-paper-dashboard-master/src/pages/Notifications.vue
+++ vue-paper-dashboard-master/vue-paper-dashboard-master/src/pages/Notifications.vue
@@ -1,165 +1,85 @@
 <template>
-  <card title="Notifications" sub-title="Custom Vue notifications plugin">
-    <div>
-      <div class="row">
-        <div class="col-md-6">
-          <h5>Notifications Style</h5>
-          <div class="alert alert-info">
-            <span>This is a plain notification</span>
-          </div>
-          <div class="alert alert-info">
-            <button type="button" aria-hidden="true" class="close">×</button>
-            <span>This is a notification with close button.</span>
-          </div>
-          <div class="alert alert-info alert-with-icon" data-notify="container">
-            <button type="button" aria-hidden="true" class="close">×</button>
-            <span data-notify="icon" class="ti-bell"></span>
-            <span data-notify="message"
-              >This is a notification with close button and icon.</span
-            >
-          </div>
-          <div class="alert alert-info alert-with-icon" data-notify="container">
-            <button type="button" aria-hidden="true" class="close">×</button>
-            <span data-notify="icon" class="ti-pie-chart"></span>
-            <span data-notify="message"
-              >This is a notification with close button and icon and have many
-              lines. You can see that the icon and the close button are always
-              vertically aligned. This is a beautiful notification. So you don't
-              have to worry about the style.</span
-            >
-          </div>
-        </div>
-        <div class="col-md-6">
-          <h5>Notification states</h5>
-          <div class="alert alert-info">
-            <button type="button" aria-hidden="true" class="close">×</button>
-            <span>
-              <b> Info - </b> This is a regular notification made with
-              ".alert-info"</span
-            >
-          </div>
-          <div class="alert alert-success">
-            <button type="button" aria-hidden="true" class="close">×</button>
-            <span>
-              <b> Success - </b> This is a regular notification made with
-              ".alert-success"</span
-            >
-          </div>
-          <div class="alert alert-warning">
-            <button type="button" aria-hidden="true" class="close">×</button>
-            <span>
-              <b> Warning - </b> This is a regular notification made with
-              ".alert-warning"</span
-            >
-          </div>
-          <div class="alert alert-danger">
-            <button type="button" aria-hidden="true" class="close">×</button>
-            <span>
-              <b> Danger - </b> This is a regular notification made with
-              ".alert-danger"</span
-            >
-          </div>
-        </div>
-      </div>
-
-      <br />
-      <br />
-
-      <div class="places-buttons">
-        <div class="row d-flex justify-content-center">
-          <div>
-            <h5>
-              Notifications Places
-              <p class="category">Click to view notifications</p>
-            </h5>
-          </div>
-        </div>
-        <div class="row d-flex justify-content-center">
-          <div class="col-md-3">
-            <p-button
-              round
-              outline
-              block
-              @click.native="notifyVue('top', 'left')"
-              >Top Left</p-button
-            >
-          </div>
-          <div class="col-md-3">
-            <p-button
-              round
-              outline
-              block
-              @click.native="notifyVue('top', 'center')"
-              >Top Center</p-button
-            >
-          </div>
-          <div class="col-md-3">
-            <p-button
-              round
-              outline
-              block
-              @click.native="notifyVue('top', 'right')"
-              >Top Right</p-button
-            >
-          </div>
-        </div>
-        <div class="row d-flex justify-content-center">
-          <div class="col-md-3">
-            <p-button
-              round
-              outline
-              block
-              @click.native="notifyVue('bottom', 'left')"
-              >Bottom Left</p-button
-            >
-          </div>
-          <div class="col-md-3">
-            <p-button
-              round
-              outline
-              block
-              @click.native="notifyVue('bottom', 'center')"
-              >Bottom Center</p-button
-            >
-          </div>
-          <div class="col-md-3">
-            <p-button
-              round
-              outline
-              block
-              @click.native="notifyVue('bottom', 'right')"
-              >Bottom Right</p-button
-            >
-          </div>
+  <div>
+    <div class="col-12">
+      <div class="card-plain">
+        <div class="table-full-width table-responsive">
+          <table class="table">
+            <thead>
+              <tr>
+                <th v-for="column in table3.columns" :key="column">{{ column }}</th>
+                <th>Apagar</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(row, index) in table3.data" :key="index">
+                <td v-for="(value, key) in row" :key="key">
+                  {{ value }}
+                </td>
+                <td>
+                  <button @click="deleteProfile(index)">Apagar</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
-  </card>
+  </div>
 </template>
-<script>
-import NotificationTemplate from "./Notifications/NotificationTemplate";
 
+<script>
 export default {
   data() {
     return {
-      type: ["", "info", "success", "warning", "danger"],
-      notifications: {
-        topCenter: false,
-      },
+      table3: {
+        title: "Table on Plain Background",
+        subTitle: "Here is a subtitle for this table",
+        columns: [
+          "Username",
+          "Email",
+          "Password",
+        ],
+        data: []
+      }
     };
   },
-  methods: {
-    notifyVue(verticalAlign, horizontalAlign) {
-      const color = Math.floor(Math.random() * 4 + 1);
-      this.$notify({
-        component: NotificationTemplate,
-        icon: "ti-gift",
-        horizontalAlign: horizontalAlign,
-        verticalAlign: verticalAlign,
-        type: this.type[color],
-      });
-    },
+  mounted() {
+    this.loadTableData();
   },
+  methods: {
+    loadTableData() {
+      const storedUsers = localStorage.getItem("users");
+      if (storedUsers) {
+        this.table3.data = JSON.parse(storedUsers);
+        this.updateNames();
+      } else {
+        this.table3.data = [
+          {
+            username: "user1",
+            email: "user1@example.com",
+            password: "password1"
+
+          },
+          {
+            username: "user2",
+            email: "user2@example.com",
+            password: "password2"
+
+          }
+        ];
+
+        localStorage.setItem("users", JSON.stringify(this.table3.data));
+      }
+    },
+    deleteProfile(index) {
+      this.table3.data.splice(index, 1);
+      localStorage.setItem("users", JSON.stringify(this.table3.data));
+    }
+  }
 };
 </script>
+
 <style></style>
+
+
+
